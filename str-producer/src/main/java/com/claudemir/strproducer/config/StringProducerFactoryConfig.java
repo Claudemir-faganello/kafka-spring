53943670eb8980ab3efe39accf00532ae1f105fb
c-faganello <=>
2023-01-15 13:27:06 -0300

    Send message with endpoint

--- str-producer/src/main/java/com/claudemir/strproducer/config/StringProducerFactoryConfig.java
+++ str-producer/src/main/java/com/claudemir/strproducer/config/StringProducerFactoryConfig.java
@@ -3,7 +3,7 @@ package com.claudemir.strproducer.config;
 import java.util.HashMap;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,8 +23,8 @@ public class StringProducerFactoryConfig {
     public ProducerFactory< String, String > producerFactory () {
         HashMap< String, Object > configs = new HashMap<>();
         configs.put( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers() );
-        configs.put( ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class );
-        configs.put( ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class );
+        configs.put( ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class );
+        configs.put( ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class );
         return new DefaultKafkaProducerFactory<>( configs );
     }
 
